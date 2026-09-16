06ec1d52be63175a9051c23385024d751797b216
Andrey Atamasov <atamasovandrey@gmail.com>
2026-09-16 14:40:06 +0700

    add infra and bootstrap.sh

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+timedatectl set-timezone Asia/Krasnoyarsk
+
+#firewall
+sudo iptables -P INPUT DROP
+sudo iptables -P FORWARD DROP
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A INPUT -p icmp -j ACCEPT
+
+
+
+
